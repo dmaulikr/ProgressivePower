585a10eb752397c6bb47706b85ca2c74d7b424c4
Michael Lee <michael_lee.org@hotmail.com>
2016-07-28 12:52:46 -0400

    Add initial view controllers

---
+++ ProgressivePower/ProgressionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProgressionViewController.swift
+//  ProgressivePower
+//
+//  Created by Michael Lee on 7/28/16.
+//  Copyright © 2016 ProgressivePower. All rights reserved.
+//
+
+import UIKit
+
+class ProgressionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
